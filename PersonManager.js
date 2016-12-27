47a053d39e75416f8b7e3643c9d768f652be675b
Sourabh Jamalapuram <sourabh.jamalapuram@gmail.com>
2016-12-27 22:12:32 +0000

    Person Manager

---
+++ PersonManager.js
@@ -0,0 +1,28 @@
+var person = {}
+
+console.log(person)
+
+person.name = "Sourabh"
+person.age = 16
+person.email = "sj@gmail.com"
+
+var person2 = {}
+person2.name = "Sruti"
+person2.age = 23
+person2.email = "srutijam@gmail.com"
+
+console.log(person.name)
+
+console.log(person2.name)
+
+var persons = []
+
+console.log(persons)
+
+persons.push(person,person2)
+
+console.log(persons)
+
+var userInput = prompt("Enter a number:")
+
+console.log(userInput)
